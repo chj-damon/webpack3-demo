ab545d253128f4a88479cfaffbf29a16e509ff5f
chenjie <chenjie@zhiweicloud.com>
2017-08-01 17:52:53 +0800

    修复正式环境下浏览器刷新导致页面404的bug；

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// 加载指定目录静态资源
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.listen(8000, () => {
-    console.log('App listening at port 8000');
+// 配置任何请求都转到index.html，而index.html会根据React-Router规则去匹配任何一个route
+app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(8888, () => {
+    console.log('App listening at port 8888');
 });
