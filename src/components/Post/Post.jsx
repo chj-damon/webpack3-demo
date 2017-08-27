c53b16246fd74d0d13bc4512eaa562ea12c2fdf5
Damon.Chen <chj_0507_dz@sina.com>
2017-08-27 16:24:18 +0800

    提交代码；

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
-import PostDetail from '../../pages/PostDetail/PostDetail';
+import { Link } from 'react-router-dom';
 import styles from './Post.css';
 
 export default props => (
     <div>
-        <h2 className={styles.title}><a href={`/post/${props.slug}`} className={styles.link}>{props.title}</a></h2>
+        <h2 className={styles.title}>
+            <Link to={`/post/${props.slug}`} className={styles.link}>{props.title}</Link>
+        </h2>
         <p className={styles.content}>{props.content}</p>
-
-        <Route path="/post/:slug" component={PostDetail} />
     </div>
 );
--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Posts from './Posts/Posts';
 import About from './About/About';
+import PostDetail from './PostDetail/PostDetail';
 import NotFound from './NotFound/NotFound';
 
 const App = () => (
@@ -13,6 +14,7 @@ const App = () => (
             <Route exact path="/" component={Posts} />
             <Route path="/posts" component={Posts} />
             <Route path="/about" component={About} />
+            <Route path="/post/:slug" component={PostDetail} />
             <Route component={NotFound} />
         </Switch>
     </div>
